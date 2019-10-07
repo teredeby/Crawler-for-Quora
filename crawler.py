e70e4dcb9b0ab2d3ef75840e39ce73a5394803bc
Débora Andrade De Lima <teredeby@gmail.com>
2019-10-06 22:00:26 -0300

    formatando saidas

--- crawler.py
+++ crawler.py
@@ -7,52 +7,62 @@ import os
 def crawl_quora_topics(topic):
 	topic='-'.join(topic.split(' '))
 	script_dir = os.getcwd()
-	rel_path = "saidas/"+topic+"/"
+	rel_path = "saidas/"+topic+".txt"
 	abs_file_path = os.path.join(script_dir, rel_path)
-	if not os.path.exists(abs_file_path):
-		os.mkdir(abs_file_path)
+	#if not os.path.exists(abs_file_path):
+	#	os.mkdir(abs_file_path)
 	print(topic)
 	url='https://pt.quora.com/topic/'+topic;
 	source_code=requests.get(url)
 	plain_text=source_code.text
 	soup=BeautifulSoup(plain_text,'html.parser')
 	i=0
+	file = open(abs_file_path,"a", encoding='utf-8')
+	file.write('\"id\",\"question\", \"answers\"')
+	file.write('\n')
+	print(len(soup.find_all('a',{'class':'question_link'})))
+	#if(len(soup.find_all('a',{'class':'question_link'})) == 0)
+	#	print('cooldown')
 	for question in soup.find_all('a',{'class':'question_link'}):
-		abs_file_path_complete = abs_file_path + str(i) + ".txt"
-		file = open(abs_file_path_complete,"a", encoding='utf-8')
 		a=''
 		print('\n')
-		
 		print('**************************NEW QUESTION************************************')
 		if(i<100):
 			i=i+1
 			print(i)
 			print('\n')
-			
+			file.write('\"'+str(i)+'\",')
 			print(question.span.get_text())
-			file.write(question.span.get_text())
+			file.write('\"'+question.span.get_text()+'\",')
 			url=question.get('href')
 			print(url)
 			source_code=requests.get(url)
 			plain_text=source_code.text
 			ansSoup=BeautifulSoup(plain_text,'html.parser')
+			txtAnswer=""
 			for answer in ansSoup.find_all('div',{'class':'ui_qtext_expanded'}):
 				print('\n')
 				print('**************************ANSWER************************************')
-				file.write('\n')
-				file.write('*A*')
-				file.write('\n')
+				#file.write('\n')
+				#file.write('*A*')
+				#file.write('\n')
 				#print(answer.prettify())
 				print(cleanhtml(answer.get_text()))
-				file.write(cleanhtml(answer.get_text()))
+				txtAnswer+=cleanhtml(answer.get_text())
+				#file.write(cleanhtml(answer.get_text()))
+			file.write('\"'+txtAnswer+'\"')
+			file.write('\n')
 		else:
 			break
-		file.close()
+	file.close()
 	
 
 def cleanhtml(raw_html):
   cleanr = re.compile('<.*?>')
   cleantext = re.sub(cleanr, '', raw_html)
+  cleanr = re.compile('"')
+  cleantext = re.sub(cleanr, '', raw_html)
+  #cleantext.replace('"', '\'')
   return cleantext
 
 topic=input("Enter Quora topics:  ")
